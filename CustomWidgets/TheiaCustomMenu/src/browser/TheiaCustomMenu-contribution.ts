74b7c9be003831876e155e0a965ac2908c2f2a9c
Shawn Boudreaux <sboudreaux@xtensible.net>
2021-06-09 11:54:55 -0400

    Multiple menu items are now togglable!

--- CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
+++ CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
@@ -1,8 +1,10 @@
 import { injectable, inject } from "inversify";
 import { Command, CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from "@theia/core/lib/common";
-import { CommonMenus } from "@theia/core/lib/browser";
-//import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
+import { CommonMenus, CommonCommands } from "@theia/core/lib/browser";
+import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
 import { WorkspaceCommands } from '@theia/workspace/lib/browser/workspace-commands';
+import { TerminalCommands } from '@theia/terminal/lib/browser/terminal-frontend-contribution';
+//import { CommonMenus } from '@theia/core/lib/browser/common-frontend-contribution';
 //import { WorkspaceCommands } from '@theia';
 
 export const TheiaCustomMenuCommand = {
@@ -21,8 +23,11 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
     menuRegistry: MenuModelRegistry;
 
     protected isEnabled: boolean = true;
-    //protected _command: Command = GettingStartedCommand;
-    protected _command: Command = WorkspaceCommands.NEW_FILE;
+    protected _command: Command = GettingStartedCommand;
+    protected _command2: Command = WorkspaceCommands.NEW_FILE;
+    protected _command3: Command = WorkspaceCommands.OPEN;
+    protected _command4: Command = TerminalCommands.NEW;
+    protected _command5: Command = CommonCommands.OPEN_PREFERENCES;
     //protected _command2: Command = WorkspaceCommands.OPEN;
 
     registerCommands(registry: CommandRegistry): void {
@@ -37,6 +42,10 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
     protected toggleMenu(isEnabled: boolean): void {
         if (isEnabled) {
             this.menuRegistry.unregisterMenuAction(this._command.id);
+            this.menuRegistry.unregisterMenuAction(this._command2.id);
+            this.menuRegistry.unregisterMenuAction(this._command3.id);
+            this.menuRegistry.unregisterMenuAction(this._command4.id);
+            this.menuRegistry.unregisterMenuAction(this._command5.id);
         } else {
             this.menuRegistry.registerMenuAction(CommonMenus.HELP, {
                 commandId: this._command.id,
