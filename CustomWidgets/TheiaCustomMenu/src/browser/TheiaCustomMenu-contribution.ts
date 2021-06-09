8e2951ec4c14c95c4303fd21c2bf4a81db4f0591
Shawn Boudreaux <sboudreaux@xtensible.net>
2021-06-09 10:45:13 -0400

    Theia Custom Menu Toggle Implementation Works!

--- CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
+++ CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
@@ -1,8 +1,8 @@
 import { injectable, inject } from "inversify";
 import { Command, CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from "@theia/core/lib/common";
 import { CommonMenus } from "@theia/core/lib/browser";
-import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
-//import { WorkspaceCommands } from '@theia/workspace/src/browser/workspace-commands';
+//import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
+import { WorkspaceCommands } from '@theia/workspace/lib/browser/workspace-commands';
 //import { WorkspaceCommands } from '@theia';
 
 export const TheiaCustomMenuCommand = {
@@ -21,8 +21,8 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
     menuRegistry: MenuModelRegistry;
 
     protected isEnabled: boolean = true;
-    protected _command: Command = GettingStartedCommand;
-    //protected _command: Command = WorkspaceCommands.ADD_FOLDER;
+    //protected _command: Command = GettingStartedCommand;
+    protected _command: Command = WorkspaceCommands.NEW_FILE;
     //protected _command2: Command = WorkspaceCommands.OPEN;
 
     registerCommands(registry: CommandRegistry): void {
