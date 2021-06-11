ea28e06c19e1c6e6d0fa360502b1846ade4e2e1a
Shawn Boudreaux <sboudreaux@xtensible.net>
2021-06-11 09:05:56 -0400

    Command Palette now disabled!

--- CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
+++ CustomWidgets/TheiaCustomMenu/src/browser/TheiaCustomMenu-contribution.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from "inversify";
 import { Command, CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from "@theia/core/lib/common";
-import { CommonMenus, CommonCommands } from "@theia/core/lib/browser";
+import { CommonMenus, CommonCommands, quickCommand, KeybindingContribution, KeybindingRegistry } from "@theia/core/lib/browser";
 import { GettingStartedCommand } from '@theia/getting-started/lib/browser/getting-started-contribution';
 import { WorkspaceCommands } from '@theia/workspace/lib/browser/workspace-commands';
 import { TerminalCommands } from '@theia/terminal/lib/browser/terminal-frontend-contribution';
@@ -16,7 +16,7 @@ export const TheiaCustomMenuCommand = {
 };
 
 @injectable()
-export class TheiaCustomMenuCommandContribution implements CommandContribution, MenuContribution {
+export class TheiaCustomMenuCommandContribution implements CommandContribution, MenuContribution, KeybindingContribution {
 
     constructor(
         // @inject(MessageService) private readonly messageService: MessageService,
@@ -33,6 +33,7 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
     protected _command5: Command = CommonCommands.OPEN_PREFERENCES;
     protected _command6: Command = KeymapsCommands.OPEN_KEYMAPS;
     protected _command7: Command = WorkspaceCommands.NEW_FOLDER;
+    protected _command8: Command = quickCommand
     //protected _command6: Command = TerminalCommands.NEW_ACTIVE_WORKSPACE;
     //protected _command6: Command = MAIN_MENU_BAR;
     //protected _command2: Command = WorkspaceCommands.OPEN;
@@ -44,7 +45,9 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
                 this.isEnabled = !this.isEnabled;
             }
         });
+        registry.unregisterCommand(this._command8.id); //unregister the quick command command
     }
+
 
     //Currently the toggleMenu function only makes the getting started menu item togglable. 
     //If other menu items want to be togglable, implement changes in the 'else' section
@@ -63,22 +66,36 @@ export class TheiaCustomMenuCommandContribution implements CommandContribution, 
             });
         }
     }
+
+    //Anything we want Custom added/removed from the menu will go in this method
     registerMenus(menus: MenuModelRegistry): void {
         menus.registerMenuAction(CommonMenus.EDIT_FIND, {
             commandId: TheiaCustomMenuCommand.id,
             label: TheiaCustomMenuCommand.label
         });
-        //this.menuRegistry.unregisterMenuAction(this._command6)
-        //this.menuRegistry.unregisterMenuAction(CommonMenus.FILE.slice(-1)[0]);
-        //this.menuRegistry.unregisterMenuAction(KeymapsCommands.OPEN_KEYMAPS.id);
+
+        //Removes whole Main Menu Items 
         this.menuRegistry.unregisterMenuAction(MonacoMenus.SELECTION.slice(-1)[0]); //Removes "Selection" from the Main menu
         this.menuRegistry.unregisterMenuAction(EditorMainMenu.GO.slice(-1)[0]); //Removes "Go" from the Main Menu
+
+        //Removes items from the "file" menu
         this.menuRegistry.unregisterMenuAction(this._command2.id); //remove "new file" from file menu
         this.menuRegistry.unregisterMenuAction(this._command7.id); //remove "new folder" from file menu
 
+        this.menuRegistry.unregisterMenuAction(this._command8.id);//unregister the quick command menu option
+    }
 
 
-
+    //unregister all keybindings associated with quick command
+    registerKeybindings(keybindings: KeybindingRegistry): void {
+        keybindings.unregisterKeybinding({
+            command: quickCommand.id,
+            keybinding: 'f1'
+        });
+        keybindings.unregisterKeybinding({
+            command: quickCommand.id,
+            keybinding: 'ctrlcmd+shift+p'
+        });
     }
 }
 
